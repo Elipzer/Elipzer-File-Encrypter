ebb245c73a1cc4e729a34e5f26a246023174a662
Elipzer <elipzation@gmail.com>
2016-03-15 21:01:19 -0400

    Shortened, Enhanced, and Cleaned Up Code
    Made the code shorter and more readable by putting it into functions.
    Removed humungous switch statements that should not have been there in
    the first place. Also put in preprocessor constants. Removed unnecessary
    arrays.

--- Simple File Encrypter/main.cpp
+++ Simple File Encrypter/main.cpp
@@ -6,6 +6,12 @@
 #include "util.h"
 #include "sha256.h"
 
+//Steaming Soup Encryption / Decryption
+
+
+#define BYTE_BITS 8
+#define BYTE_FIRST_BIT 0x80
+
 unsigned char getHexChar(std::string value)
 {
 	std::stringstream ss;
@@ -24,312 +30,131 @@ std::vector<unsigned char> getHexChars(std::string values)
 	return ret;
 }
 
-std::vector<unsigned int> getMoveKey(const std::string& hash, std::vector<unsigned int>& movekey, unsigned int slotDepth)
+void getMoveKey(const std::string& hash, std::vector<unsigned char>& movekey, unsigned int rows)
 {
-	std::vector<unsigned int> slots;
-	slots.reserve(8 * slotDepth);
+	std::vector<unsigned char> slots;
+	slots.reserve(BYTE_BITS * rows);
 
-	for (unsigned int i = 0; i < 8 * slotDepth; i++)
+	for (unsigned char i = 0; i < BYTE_BITS * rows; i++)
 		slots.push_back(i);
 
 	std::vector<unsigned char> digest = getHexChars(hash);
 	movekey.clear();
-	movekey.reserve(8 * slotDepth);
+	movekey.reserve(BYTE_BITS * rows);
 
-	for (unsigned int o = 0; o < 8 * slotDepth; o++)
+	for (unsigned int i = 0; i < BYTE_BITS * rows; i++)
 	{
-		unsigned int dgst = digest[o];
-		unsigned int dgstm = dgst % slots.size();
-		unsigned int slts = slots[dgstm];
+		unsigned char dgstm = digest[i] % slots.size();
+		unsigned char slts = slots[dgstm];
 		slots.erase(slots.begin() + dgstm);
 		movekey.push_back(slts);
 	}
+}
 
-	return movekey;
+std::vector<unsigned char> generateBits(unsigned char* bytes, unsigned int rows)
+{
+	std::vector<unsigned char> bits;
+	bits.reserve(BYTE_BITS * rows);
+
+	for (unsigned int i = 0; i < BYTE_BITS * rows; i++)
+		bits.push_back(bytes[i / BYTE_BITS] & (BYTE_FIRST_BIT >> (i % BYTE_BITS)));
+	
+	return bits;
+}
+
+std::vector<unsigned char> generateEncryptedBytes(std::string& hash, unsigned char* bytes, unsigned int rows)
+{
+	std::vector<unsigned char> bits = generateBits(bytes, rows);
+
+	hash = sha256(hash);
+
+	std::vector<unsigned char> movekey;
+	getMoveKey(hash, movekey, rows);
+
+	std::vector<unsigned char> newBytes;
+	newBytes.resize(rows, 0x00);
+
+	for (unsigned int o = 0; o < rows; o++)
+		for (unsigned int u = 0; u < BYTE_BITS; u++)
+			newBytes[o] += ((bits[movekey[o * BYTE_BITS + u]] ? 1 : 0) * (BYTE_FIRST_BIT >> u));
+
+	return newBytes;
 }
 
 void encrypt(std::vector<unsigned char> data, std::string password, std::vector<unsigned char>& outBytes)
 {
+	const static unsigned int DEFAULT_ROWS = 4;
+
 	std::string hash = sha256(password);
 	outBytes.reserve(data.size());
 
-	//Normal Part
-	for (unsigned int i = 0; i < (unsigned int)(data.size() / 4); i++)
+	for (unsigned int i = 0; i < (unsigned int)(data.size() / DEFAULT_ROWS); i++)
 	{
-		unsigned char* bytes_i = &(data[0]) + i * 4;
-
-		//each bit in the bytes has its own number as described above.
-
-		//Possibly faster if not using bools but just store the char values and not have to do 32 != operations
-
-		std::vector<bool> bits = {
-			(bytes_i[0] & 0x80) != 0, (bytes_i[0] & 0x40) != 0, (bytes_i[0] & 0x20) != 0, (bytes_i[0] & 0x10) != 0, (bytes_i[0] & 0x08) != 0, (bytes_i[0] & 0x04) != 0, (bytes_i[0] & 0x02) != 0, (bytes_i[0] & 0x01) != 0,
-			(bytes_i[1] & 0x80) != 0, (bytes_i[1] & 0x40) != 0, (bytes_i[1] & 0x20) != 0, (bytes_i[1] & 0x10) != 0, (bytes_i[1] & 0x08) != 0, (bytes_i[1] & 0x04) != 0, (bytes_i[1] & 0x02) != 0, (bytes_i[1] & 0x01) != 0,
-			(bytes_i[2] & 0x80) != 0, (bytes_i[2] & 0x40) != 0, (bytes_i[2] & 0x20) != 0, (bytes_i[2] & 0x10) != 0, (bytes_i[2] & 0x08) != 0, (bytes_i[2] & 0x04) != 0, (bytes_i[2] & 0x02) != 0, (bytes_i[2] & 0x01) != 0,
-			(bytes_i[3] & 0x80) != 0, (bytes_i[3] & 0x40) != 0, (bytes_i[3] & 0x20) != 0, (bytes_i[3] & 0x10) != 0, (bytes_i[3] & 0x08) != 0, (bytes_i[3] & 0x04) != 0, (bytes_i[3] & 0x02) != 0, (bytes_i[3] & 0x01) != 0,
-		};
-
-		std::vector<unsigned int> slots = {
-			0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-			0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-			0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-			0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
-		};
-
-		hash = sha256(hash);
-
-		std::vector<unsigned int> movekey;
-		getMoveKey(hash, movekey, 4);
-
-		std::vector<unsigned char> newBytes;
-		newBytes.resize(4, 0x00);
-
-		for (unsigned int o = 0; o < 4; o++)
-		{
-			for (unsigned int u = 0; u < 8; u++)
-			{
-				newBytes[o] += ((bits[movekey[o * 8 + u]] ? 1 : 0) * (0x80 >> u));
-			}
-		}
-
-		for (unsigned int o = 0; o < newBytes.size(); o++)
-			outBytes.push_back(newBytes[o]);
+		std::vector<unsigned char> encrypted = generateEncryptedBytes(hash, data.data() + i * DEFAULT_ROWS, DEFAULT_ROWS);
+		outBytes.insert(outBytes.end(), encrypted.begin(), encrypted.end());
 	}
 
-	//Extra stuff at the end of the file if it is not a perfect length
-	switch (data.size() % 4)
+	unsigned int extraEnd = data.size() % DEFAULT_ROWS;
+	if (extraEnd > 0)
 	{
-	case 0:
-		break;
-	case 1:
+		std::vector<unsigned char> encrypted = generateEncryptedBytes(hash, data.data() + data.size() - extraEnd, extraEnd);
+		outBytes.insert(outBytes.end(), encrypted.begin(), encrypted.end());
+	}
+}
+
+std::vector<unsigned char> generateDecryptedBytes(std::string& hash, unsigned char* bytes, unsigned int rows)
+{
+	hash = sha256(hash);
+
+	std::vector<unsigned char> movekey;
+	getMoveKey(hash, movekey, rows);
+
+	std::vector<unsigned char> newBytes;
+	newBytes.resize(rows, 0x00);
+
+	for (unsigned int o = 0; o < movekey.size(); o++)
 	{
-		unsigned char* bytes_i = &(data[0]) + data.size() - 1;
-
-		//each bit in the bytes has its own number as described above.
-
-		//Possibly faster if not using bools but just store the char values and not have to do 32 != operations
-
-		std::vector<bool> bits = {
-			(bytes_i[0] & 0x80) != 0, (bytes_i[0] & 0x40) != 0, (bytes_i[0] & 0x20) != 0, (bytes_i[0] & 0x10) != 0, (bytes_i[0] & 0x08) != 0, (bytes_i[0] & 0x04) != 0, (bytes_i[0] & 0x02) != 0, (bytes_i[0] & 0x01) != 0,
-		};
-
-		hash = sha256(hash);
-
-		std::vector<unsigned int> movekey;
-		getMoveKey(hash, movekey, 1);
-
-		std::vector<unsigned char> newBytes;
-		newBytes.resize(1, 0x00);
-
-		for (unsigned int o = 0; o < 1; o++)
-		{
-			for (unsigned int u = 0; u < 8; u++)
-			{
-				newBytes[o] += ((bits[movekey[o * 8 + u]] ? 1 : 0) * (0x80 >> u));
-			}
-		}
-
-		for (unsigned int o = 0; o < newBytes.size(); o++)
-			outBytes.push_back(newBytes[o]);
-		break;
+		unsigned int mvko = movekey[o];
+		newBytes[mvko / BYTE_BITS] += ((bytes[o / BYTE_BITS] & (BYTE_FIRST_BIT >> o % BYTE_BITS)) ? 1 : 0) * (BYTE_FIRST_BIT >> (mvko % BYTE_BITS));
 	}
-	case 2:
-	{
-		unsigned char* bytes_i = &(data[0]) + data.size() - 2;
 
-		//each bit in the bytes has its own number as described above.
-
-		//Possibly faster if not using bools but just store the char values and not have to do 32 != operations
-
-		std::vector<bool> bits = {
-			(bytes_i[0] & 0x80) != 0, (bytes_i[0] & 0x40) != 0, (bytes_i[0] & 0x20) != 0, (bytes_i[0] & 0x10) != 0, (bytes_i[0] & 0x08) != 0, (bytes_i[0] & 0x04) != 0, (bytes_i[0] & 0x02) != 0, (bytes_i[0] & 0x01) != 0,
-			(bytes_i[1] & 0x80) != 0, (bytes_i[1] & 0x40) != 0, (bytes_i[1] & 0x20) != 0, (bytes_i[1] & 0x10) != 0, (bytes_i[1] & 0x08) != 0, (bytes_i[1] & 0x04) != 0, (bytes_i[1] & 0x02) != 0, (bytes_i[1] & 0x01) != 0,
-		};
-
-		hash = sha256(hash);
-
-		std::vector<unsigned int> movekey;
-		getMoveKey(hash, movekey, 2);
-
-		std::vector<unsigned char> newBytes;
-		newBytes.resize(2, 0x00);
-
-		for (unsigned int o = 0; o < 2; o++)
-		{
-			for (unsigned int u = 0; u < 8; u++)
-			{
-				newBytes[o] += ((bits[movekey[o * 8 + u]] ? 1 : 0) * (0x80 >> u));
-			}
-		}
-
-		for (unsigned int o = 0; o < newBytes.size(); o++)
-			outBytes.push_back(newBytes[o]);
-		break;
-	}
-	case 3:
-	{
-		unsigned char* bytes_i = &(data[0]) + data.size() - 3;
-
-		//each bit in the bytes has its own number as described above.
-
-		//Possibly faster if not using bools but just store the char values and not have to do 32 != operations
-
-		std::vector<bool> bits = {
-			(bytes_i[0] & 0x80) != 0, (bytes_i[0] & 0x40) != 0, (bytes_i[0] & 0x20) != 0, (bytes_i[0] & 0x10) != 0, (bytes_i[0] & 0x08) != 0, (bytes_i[0] & 0x04) != 0, (bytes_i[0] & 0x02) != 0, (bytes_i[0] & 0x01) != 0,
-			(bytes_i[1] & 0x80) != 0, (bytes_i[1] & 0x40) != 0, (bytes_i[1] & 0x20) != 0, (bytes_i[1] & 0x10) != 0, (bytes_i[1] & 0x08) != 0, (bytes_i[1] & 0x04) != 0, (bytes_i[1] & 0x02) != 0, (bytes_i[1] & 0x01) != 0,
-			(bytes_i[2] & 0x80) != 0, (bytes_i[2] & 0x40) != 0, (bytes_i[2] & 0x20) != 0, (bytes_i[2] & 0x10) != 0, (bytes_i[2] & 0x08) != 0, (bytes_i[2] & 0x04) != 0, (bytes_i[2] & 0x02) != 0, (bytes_i[2] & 0x01) != 0,
-		};
-
-		hash = sha256(hash);
-
-		std::vector<unsigned int> movekey;
-		getMoveKey(hash, movekey, 3);
-
-		std::vector<unsigned char> newBytes;
-		newBytes.resize(3, 0x00);
-
-		for (unsigned int o = 0; o < 3; o++)
-		{
-			for (unsigned int u = 0; u < 8; u++)
-			{
-				newBytes[o] += ((bits[movekey[o * 8 + u]] ? 1 : 0) * (0x80 >> u));
-			}
-		}
-
-		for (unsigned int o = 0; o < newBytes.size(); o++)
-			outBytes.push_back(newBytes[o]);
-		break;
-	}
-	default:
-		exit(-4);
-		break;
-	}
+	return newBytes;
 }
 
 void decrypt(std::vector<unsigned char> data, std::string password, std::vector<unsigned char>& outBytes)
 {
+	const static unsigned int DEFAULT_ROWS = 4;
+
 	std::string hash = sha256(password);
 	outBytes.reserve(data.size());
 
-	//Normal Part
-	for (unsigned int i = 0; i < (unsigned int)(data.size() / 4); i++)
+	for (unsigned int i = 0; i < (unsigned int)(data.size() / DEFAULT_ROWS); i++)
 	{
-		unsigned char* bytes_i = &(data[0]) + i * 4;
-
-		hash = sha256(hash);
-
-		std::vector<unsigned int> movekey;
-		getMoveKey(hash, movekey, 4);
-
-		std::vector<unsigned char> newBytes;
-		newBytes.resize(4, 0x00);
-
-		for (unsigned int o = 0; o < movekey.size(); o++)
-		{
-			//Find slot and put it there.
-			unsigned int mvko = movekey[o];
-			newBytes[mvko / 8] += ((bytes_i[o / 8] & (0x80 >> o % 8)) ? 1 : 0) * (0x80 >> (mvko % 8));
-		}
-
-		for (unsigned int o = 0; o < newBytes.size(); o++)
-			outBytes.push_back(newBytes[o]);
+		std::vector<unsigned char> decrypted = generateDecryptedBytes(hash, data.data() + i * DEFAULT_ROWS, DEFAULT_ROWS);
+		outBytes.insert(outBytes.end(), decrypted.begin(), decrypted.end());
 	}
 
-	//Extra stuff at the end of the file if it is not a perfect length
-	switch (data.size() % 4)
+	unsigned int extraEnd = data.size() % DEFAULT_ROWS;
+	if (extraEnd > 0)
 	{
-	case 0:
-		break;
-	case 1:
-	{
-		unsigned char* bytes_i = &(data[0]) + data.size() - 1;
-
-		//each bit in the bytes has its own number as described above.
-
-		//Possibly faster if not using bools but just store the char values and not have to do 32 != operations
-
-		hash = sha256(hash);
-
-		std::vector<unsigned int> movekey;
-		getMoveKey(hash, movekey, 1);
-
-		std::vector<unsigned char> newBytes;
-		newBytes.resize(1, 0x00);
-
-		for (unsigned int o = 0; o < movekey.size(); o++)
-		{
-			//Find slot and put it there.
-			unsigned int mvko = movekey[o];
-			newBytes[mvko / 8] += ((bytes_i[o / 8] & (0x80 >> o % 8)) ? 1 : 0) * (0x80 >> (mvko % 8));
-		}
-
-		for (unsigned int o = 0; o < newBytes.size(); o++)
-			outBytes.push_back(newBytes[o]);
-		break;
-	}
-	case 2:
-	{
-		unsigned char* bytes_i = &(data[0]) + data.size() - 2;
-
-		hash = sha256(hash);
-
-		std::vector<unsigned int> movekey;
-		getMoveKey(hash, movekey, 2);
-
-		std::vector<unsigned char> newBytes;
-		newBytes.resize(2, 0x00);
-
-		for (unsigned int o = 0; o < movekey.size(); o++)
-		{
-			//Find slot and put it there.
-			unsigned int mvko = movekey[o];
-			newBytes[mvko / 8] += ((bytes_i[o / 8] & (0x80 >> o % 8)) ? 1 : 0) * (0x80 >> (mvko % 8));
-		}
-
-		for (unsigned int o = 0; o < newBytes.size(); o++)
-			outBytes.push_back(newBytes[o]);
-		break;
-	}
-	case 3:
-	{
-		unsigned char* bytes_i = &(data[0]) + data.size() - 3;
-
-		hash = sha256(hash);
-
-		std::vector<unsigned int> movekey;
-		getMoveKey(hash, movekey, 3);
-
-		std::vector<unsigned char> newBytes;
-		newBytes.resize(3, 0x00);
-
-		for (unsigned int o = 0; o < movekey.size(); o++)
-		{
-			//Find slot and put it there.
-			unsigned int mvko = movekey[o];
-			newBytes[mvko / 8] += ((bytes_i[o / 8] & (0x80 >> o % 8)) ? 1 : 0) * (0x80 >> (mvko % 8));
-		}
-
-		for (unsigned int o = 0; o < newBytes.size(); o++)
-			outBytes.push_back(newBytes[o]);
-		break;
-	}
-	default:
-		exit(-4);
-		break;
+		std::vector<unsigned char> decrypted = generateDecryptedBytes(hash, data.data() + data.size() - extraEnd, extraEnd);
+		outBytes.insert(outBytes.end(), decrypted.begin(), decrypted.end());
 	}
 }
 
 bool endswith(const std::string& fullString, const std::string& ending)
 {
 	if (fullString.length() >= ending.length())
-		return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
+		return fullString.compare(fullString.length() - ending.length(), ending.length(), ending) == 0;
 	else
 		return false;
 }
 
 int main(int argc, char *argv[])
 {
-	SetConsoleTitle("Elipzer Encrypter / Decrypter v1.0.0");
+	std::string name = "Elipzer Encrypter / Decrypter v 1.0.0";
+
+	SetConsoleTitle(name.c_str());
 
 	if (argc < 2)
 	{
@@ -338,7 +163,7 @@ int main(int argc, char *argv[])
 		return -6;
 	}
 
-	std::string fileName = std::string(argv[1]);//"C:/Users/Elipzer/Development/VS 2015/Simple File Encrypter/Debug/toencrypt.txt.epzcrypto";
+	std::string fileName = std::string(argv[1]);
 	std::string extension = ".epzcrypto";
 
 	std::cout << "File:" << std::endl << fileName << std::endl << std::endl;
@@ -347,7 +172,7 @@ int main(int argc, char *argv[])
 	{
 		//Decrypt
 
-		std::cout << "Decrypting File Using Elipzer Encrypter/Decrypter v1.0.0" << std::endl << std::endl;
+		std::cout << "Decrypting File Using " << name << std::endl << std::endl;
 
 		std::string outFileName = fileName.substr(0, fileName.length() - extension.length());
 
@@ -385,7 +210,7 @@ int main(int argc, char *argv[])
 	{
 		//Encrypt
 
-		std::cout << "Encrypting File Using Elipzer Encrypter/Decrypter v1.0.0" << std::endl << std::endl;
+		std::cout << "Encrypting File Using " << name << std::endl << std::endl;
 
 		std::string outFileName = fileName + extension;
 
@@ -418,8 +243,6 @@ int main(int argc, char *argv[])
 
 		std::cout << "Encryption Complete" << std::endl << std::endl << "Saved to:" << std::endl << std::endl << outFileName << std::endl << std::endl;
 	}
-
-	
 
 	system("PAUSE");
 
