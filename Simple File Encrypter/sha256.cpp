f0c438549fc2e505be172b758bb5d2418ae25e0a
Elipzer <elipzation@gmail.com>
2016-03-24 13:36:03 -0400

    Created My Own SHA-256 Code

--- Simple File Encrypter/sha256.cpp
+++ Simple File Encrypter/sha256.cpp
@@ -1,140 +1,169 @@
-#include <cstring>
-#include <fstream>
+#include <vector>
+#include <iostream>
 #include "sha256.h"
 
-const unsigned int SHA256::sha256_k[64] = //UL = uint32
-{ 0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2 };
-
-void SHA256::transform(const unsigned char *message, unsigned int block_nb)
+int Message::init(const std::vector<unsigned char>& bytes)
 {
-	uint32 w[64];
-	uint32 wv[8];
-	uint32 t1, t2;
-	const unsigned char *sub_block;
-	int i;
-	int j;
-	for (i = 0; i < (int)block_nb; i++)
+	//Bytes not correct size
+	if (bytes.size() % 64 != 0)
+		return -1;
+
+	blocks.reserve(bytes.size() / 64);
+
+	for (unsigned int i = 0; i < bytes.size() / 64; i++)
 	{
-		sub_block = message + (i << 6);
-		for (j = 0; j < 16; j++)
-		{
-			SHA2_PACK32(&sub_block[j << 2], &w[j]);
-		}
-		for (j = 16; j < 64; j++)
-		{
-			w[j] = SHA256_F4(w[j - 2]) + w[j - 7] + SHA256_F3(w[j - 15]) + w[j - 16];
-		}
-		for (j = 0; j < 8; j++)
-		{
-			wv[j] = m_h[j];
-		}
-		for (j = 0; j < 64; j++)
-		{
-			t1 = wv[7] + SHA256_F2(wv[4]) + SHA2_CH(wv[4], wv[5], wv[6])
-				+ sha256_k[j] + w[j];
-			t2 = SHA256_F1(wv[0]) + SHA2_MAJ(wv[0], wv[1], wv[2]);
-			wv[7] = wv[6];
-			wv[6] = wv[5];
-			wv[5] = wv[4];
-			wv[4] = wv[3] + t1;
-			wv[3] = wv[2];
-			wv[2] = wv[1];
-			wv[1] = wv[0];
-			wv[0] = t1 + t2;
-		}
-		for (j = 0; j < 8; j++)
-		{
-			m_h[j] += wv[j];
-		}
+		blocks.push_back(MessageBlock((unsigned char*)(bytes.data() + i * 64)));
 	}
+
+	return 0;
 }
 
-void SHA256::init()
+unsigned int Message::getMessageBlockCount() const
 {
-	m_h[0] = 0x6a09e667;
-	m_h[1] = 0xbb67ae85;
-	m_h[2] = 0x3c6ef372;
-	m_h[3] = 0xa54ff53a;
-	m_h[4] = 0x510e527f;
-	m_h[5] = 0x9b05688c;
-	m_h[6] = 0x1f83d9ab;
-	m_h[7] = 0x5be0cd19;
-	m_len = 0;
-	m_tot_len = 0;
+	return (unsigned int)blocks.size();
 }
 
-void SHA256::update(const unsigned char *message, unsigned int len)
+const MessageBlock& Message::getMessageBlock(unsigned int i) const
 {
-	unsigned int block_nb;
-	unsigned int new_len, rem_len, tmp_len;
-	const unsigned char *shifted_message;
-	tmp_len = SHA224_256_BLOCK_SIZE - m_len;
-	rem_len = len < tmp_len ? len : tmp_len;
-	memcpy(&m_block[m_len], message, rem_len);
-	if (m_len + len < SHA224_256_BLOCK_SIZE)
-	{
-		m_len += len;
-		return;
-	}
-	new_len = len - rem_len;
-	block_nb = new_len / SHA224_256_BLOCK_SIZE;
-	shifted_message = message + rem_len;
-	transform(m_block, 1);
-	transform(shifted_message, block_nb);
-	rem_len = new_len % SHA224_256_BLOCK_SIZE;
-	memcpy(m_block, &shifted_message[block_nb << 6], rem_len);
-	m_len = rem_len;
-	m_tot_len += (block_nb + 1) << 6;
+	return blocks[i];
 }
 
-void SHA256::final(unsigned char *digest)
+MessageSchedule::MessageSchedule(Message m, uint32_t i)
 {
-	unsigned int block_nb;
-	unsigned int pm_len;
-	unsigned int len_b;
-	int i;
-	block_nb = (1 + ((SHA224_256_BLOCK_SIZE - 9)
-		< (m_len % SHA224_256_BLOCK_SIZE)));
-	len_b = (m_tot_len + m_len) << 3;
-	pm_len = block_nb << 6;
-	memset(m_block + m_len, 0, pm_len - m_len);
-	m_block[m_len] = 0x80;
-	SHA2_UNPACK32(len_b, m_block + pm_len - 4);
-	transform(m_block, block_nb);
-	for (i = 0; i < 8; i++)
-	{
-		SHA2_UNPACK32(m_h[i], &digest[i << 2]);
-	}
+	//64 32-bit words = 2048
+	//First 16 are Mt(i)
+	for (unsigned int t = 0; t < 16; t++)
+		data[t] = MessageWord(m.getMessageBlock(i).getWord(t).getData());
+	//17-64 are o1(Wt-2) + Wt-7 + 0(Wt-15) + Wt-16
+	for (unsigned int t = 16; t < 64; t++)
+		data[t] = MessageWord(
+			o1(data[t - 2].getData()) +
+			data[t - 7].getData() +
+			o0(data[t - 15].getData()) +
+			data[t - 16].getData()
+			);
 }
 
 std::string sha256(std::string input)
 {
-	unsigned char digest[SHA256::DIGEST_SIZE];
-	memset(digest, 0, SHA256::DIGEST_SIZE);
+	//Initial Hash Values
+	uint32_t h[] = {
+		0x6a09e667,
+		0xbb67ae85,
+		0x3c6ef372,
+		0xa54ff53a,
+		0x510e527f,
+		0x9b05688c,
+		0x1f83d9ab,
+		0x5be0cd19,
+	};
 
-	SHA256 ctx = SHA256();
-	ctx.init();
-	ctx.update((unsigned char*)input.c_str(), (unsigned int)input.length());
-	ctx.final(digest);
+	//Round Constants
+	uint32_t k[] = {
+		0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+	};
 
-	char buf[2 * SHA256::DIGEST_SIZE + 1];
-	buf[2 * SHA256::DIGEST_SIZE] = 0;
-	for (int i = 0; i < SHA256::DIGEST_SIZE; i++)
+	//Copy input to a vector of chars
+	std::vector<unsigned char> message(input.begin(), input.end());
+
+	//Append bit "1" to message
+
+	//This algorithm also automagically adds 7 "0"s to the message (after the 1 bit) because it is working with bytes
+	//and not bits. (This will never be a problem as all unsigned chars are bytes)
+	unsigned char firstBit(0x80);
+	message.push_back(firstBit);
+
+	//Make the message's size % 64 = 56  by adding chars.
+	uint32_t sizeMod64 = message.size() % 64;
+	uint32_t extraToAdd;
+	if (sizeMod64 > 56)
+		extraToAdd = 120 - sizeMod64;// (Amount to Add to get to Next Block + 56) = (64 - sizeMod64 + 56) = (120 - sizeMod64)
+	else
+		extraToAdd = 56 - sizeMod64;
+	
+	//Insert the zeros.
+	message.resize(message.size() + extraToAdd);
+
+	//Create an array of bytes that will be the size
+	byte sizeArr[8];
+	unsigned long long size = input.size() * 8;
+	
+	//Convert the long long size into bytes
+	for (uint32_t i = 0; i < 8; i++)
+		sizeArr[7 - i] = (byte)(size >> (i * 8));
+
+	//Insert the size bytes into the message.
+	message.insert(message.end(), sizeArr, sizeArr + 8);
+
+	//Create the message
+	Message mess;
+	int err = mess.init(message);
+
+	//Check for errors with the message calculation (Only checks for if it is for some reason
+	//(which should never exist) that the message is the wrong length)
+	if (err != 0)
+	{
+		std::cout << "Invalid Message" << std::endl;
+		return "Invalid Message";
+	}
+
+	//Compute Hash
+	for (uint32_t i = 0; i < mess.getMessageBlockCount(); i++)
+	{
+		//Create a temporary variable, h_tmp with values equal to m_h 
+		uint32_t h_tmp[8];
+		for (unsigned int i = 0; i < 8; i++)
+			h_tmp[i] = h[i];
+
+		//Create the Message Schedule
+		MessageSchedule W(mess, i);
+
+		//Compute the values to be added to the hash
+		for (unsigned int t = 0; t < 64; t++)
+		{
+			uint32_t t1 = h_tmp[7] + E1(h_tmp[4]) + Ch(h_tmp[4], h_tmp[5], h_tmp[6]) + k[t] + W.getWord(t).getData();
+			uint32_t t2 = E0(h_tmp[0]) + Maj(h_tmp[0], h_tmp[1], h_tmp[2]);
+			h_tmp[7] = h_tmp[6];
+			h_tmp[6] = h_tmp[5];
+			h_tmp[5] = h_tmp[4];
+			h_tmp[4] = h_tmp[3] + t1;
+			h_tmp[3] = h_tmp[2];
+			h_tmp[2] = h_tmp[1];
+			h_tmp[1] = h_tmp[0];
+			h_tmp[0] = t1 + t2;
+		}
+
+		//Create the next version of the hash
+		for (unsigned int i = 0; i < 8; i++)
+		{
+			h[i] = h_tmp[i] + h[i];
+		}
+	}
+
+	//Create Digest
+	byte digest[32];
+
+	//Put the values into the digest.
+	for (unsigned int i = 0; i < 8; i++)
+	{
+		UINT32_T_TO_BYTE_ARRAY(h[i], digest + i * 4);
+	}
+
+	//Convert h to a string
+	//32 for digest size, 2 chars per digest byte (0xXX), 1 for null termination at end ('\0')
+	char buf[65];
+	buf[64] = '\0';
+
+	for (unsigned int i = 0; i < 32; i++)
 		sprintf_s(buf + i * 2, 3, "%02x", digest[i]);
+
+	//Return the Computed Hash String (In hexadecimal)
 	return std::string(buf);
 }
--- Simple File Encrypter/sha256.h
+++ Simple File Encrypter/sha256.h
@@ -1,90 +1,109 @@
-#ifndef SHA256_H
-#define SHA256_H
+#pragma once
+
 #include <string>
 
-/*
-* Updated to C++, zedwood.com 2012
-* Based on Olivier Gay's version
-* See Modified BSD License below:
-*
-* FIPS 180-2 SHA-224/256/384/512 implementation
-* Issue date:  04/30/2005
-* http://www.ouah.org/ogay/sha2/
-*
-* Copyright (C) 2005, 2007 Olivier Gay <olivier.gay@a3.epfl.ch>
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions
-* are met:
-* 1. Redistributions of source code must retain the above copyright
-*    notice, this list of conditions and the following disclaimer.
-* 2. Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in the
-*    documentation and/or other materials provided with the distribution.
-* 3. Neither the name of the project nor the names of its contributors
-*    may be used to endorse or promote products derived from this software
-*    without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE PROJECT AND CONTRIBUTORS ''AS IS'' AND
-* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* ARE DISCLAIMED.  IN NO EVENT SHALL THE PROJECT OR CONTRIBUTORS BE LIABLE
-* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-* SUCH DAMAGE.
-*/
+typedef unsigned char byte;
 
-class SHA256
+#define ROTR(x, y) ((x >> y) | (x << ((sizeof(x) << 3) - y)))
+#define ROTL(x, y) ((x << y) | (x >> ((sizeof(x) << 3) - y)))
+
+#define Ch(x, y, z) ((x & y) ^ (~x & z))
+#define Maj(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
+
+#define E0(x) ((ROTR(x, 2)) ^ (ROTR(x, 13)) ^ (ROTR(x, 22)))
+#define E1(x) ((ROTR(x, 6)) ^ (ROTR(x, 11)) ^ (ROTR(x, 25)))
+#define o0(x) ((ROTR(x, 7)) ^ (ROTR(x, 18)) ^ (x >> 3))
+#define o1(x) ((ROTR(x, 17)) ^ (ROTR(x, 19)) ^ (x >> 10))
+
+#define BYTE_ARRAY_TO_UINT32_T(x) ((x[0] << 24) + (x[1] << 16) + (x[2] << 8) + x[3])
+#define UINT32_T_TO_BYTE_ARRAY(x, arr)       \
+{                                            \
+	*(arr + 3) = (unsigned char)(x      );   \
+	*(arr + 2) = (unsigned char)(x >> 8 );   \
+	*(arr + 1) = (unsigned char)(x >> 16);   \
+	*(arr + 0) = (unsigned char)(x >> 24);   \
+}
+
+//32 bits per word
+class MessageWord
 {
-protected:
-	typedef unsigned char uint8;
-	typedef unsigned int uint32;
-	typedef unsigned long long uint64;
-
-	const static uint32 sha256_k[];
-	static const unsigned int SHA224_256_BLOCK_SIZE = (512 / 8);
 public:
-	void init();
-	void update(const unsigned char *message, unsigned int len);
-	void final(unsigned char *digest);
-	static const unsigned int DIGEST_SIZE = (256 / 8);
+	MessageWord(const byte* bytes)
+	{
+		memcpy(data, bytes, 4);
+	}
 
-protected:
-	void transform(const unsigned char *message, unsigned int block_nb);
-	unsigned int m_tot_len;
-	unsigned int m_len;
-	unsigned char m_block[2 * SHA224_256_BLOCK_SIZE];
-	uint32 m_h[8];
+	MessageWord(uint32_t bytes)
+	{
+		byte tmp[4];
+		memcpy(tmp, &bytes, 4);
+		for (unsigned int i = 0; i < 4; i++)
+			data[3 - i] = tmp[i];
+	}
+
+	MessageWord()
+	{
+		memset(data, 0, 4);
+	}
+
+	byte data[4];
+
+	uint32_t getData() const
+	{
+		return BYTE_ARRAY_TO_UINT32_T(data);
+	}
 };
 
-std::string sha256(std::string input);
+//512 bits per block
+class MessageBlock
+{
+public:
+	MessageBlock(unsigned char* bytes)
+	{
+		//assumes 64 byte block
+		//64 / 4 = 16 words per block
+		for (uint32_t i = 0; i < 16; i++)
+			data[i] = MessageWord(bytes + i * 4);
+	}
 
-#define SHA2_SHFR(x, n)    (x >> n)
-#define SHA2_ROTR(x, n)   ((x >> n) | (x << ((sizeof(x) << 3) - n)))
-#define SHA2_ROTL(x, n)   ((x << n) | (x >> ((sizeof(x) << 3) - n)))
-#define SHA2_CH(x, y, z)  ((x & y) ^ (~x & z))
-#define SHA2_MAJ(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
-#define SHA256_F1(x) (SHA2_ROTR(x,  2) ^ SHA2_ROTR(x, 13) ^ SHA2_ROTR(x, 22))
-#define SHA256_F2(x) (SHA2_ROTR(x,  6) ^ SHA2_ROTR(x, 11) ^ SHA2_ROTR(x, 25))
-#define SHA256_F3(x) (SHA2_ROTR(x,  7) ^ SHA2_ROTR(x, 18) ^ SHA2_SHFR(x,  3))
-#define SHA256_F4(x) (SHA2_ROTR(x, 17) ^ SHA2_ROTR(x, 19) ^ SHA2_SHFR(x, 10))
-#define SHA2_UNPACK32(x, str)                 \
-{                                             \
-    *((str) + 3) = (uint8) ((x)      );       \
-    *((str) + 2) = (uint8) ((x) >>  8);       \
-    *((str) + 1) = (uint8) ((x) >> 16);       \
-    *((str) + 0) = (uint8) ((x) >> 24);       \
-}
-#define SHA2_PACK32(str, x)                   \
-{                                             \
-    *(x) =   ((uint32) *((str) + 3)      )    \
-           | ((uint32) *((str) + 2) <<  8)    \
-           | ((uint32) *((str) + 1) << 16)    \
-           | ((uint32) *((str) + 0) << 24);   \
-}
-#endif
+	//32 bits per word (4 unsigned chars)
+	const MessageWord& getWord(unsigned int j) const
+	{
+		return data[j];
+	}
+
+private:
+	MessageWord data[16];
+};
+
+//Varying amount of blocks per message
+class Message
+{
+public:
+	int init(const std::vector<unsigned char>& bytes);
+
+	unsigned int getMessageBlockCount() const;
+	
+	const MessageBlock& getMessageBlock(unsigned int i) const;
+
+private:
+	std::vector<MessageBlock> blocks;
+};
+
+//2048 bits per schedule
+class MessageSchedule
+{
+public:
+	MessageSchedule(Message m, uint32_t i);
+
+	const MessageWord& getWord(unsigned int j) const
+	{
+		return data[j];
+	}
+
+private:
+	MessageWord data[64];
+};
+
+//Returns the sha256 hash of a string, input
+extern std::string sha256(std::string input);
